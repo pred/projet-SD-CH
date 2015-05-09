300a7bd3effdeb76546658b43bbcfab6cc96f017
rorchar <simon.hamant@gmail.com>
2015-05-09 15:47:52 +0200

    test de git

--- src/sd/Film.java
+++ src/sd/Film.java
@@ -4,20 +4,24 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Film {
-	String Titre;
-	int Année;
-	String Type;
-	String Synopsis;
-	String Directeur;
-	List<String> Acteurs = new ArrayList<String>();
-	List<String> Genre = new ArrayList<String>();
+	private String Titre;
+	private int Année;
+	private String Type;
+	private String Synopsis;
+	private Directeur Directeur;
+	private List<Acteur> Acteurs = new ArrayList<Acteur>();
+	private List<Genre> Genre = new ArrayList<Genre>();
+	private String Duree;
 	
 	public Film() {
 		Titre = "inconnu";
 		Année = 0;
 		Type = "inconnu";
 		Synopsis = "inconnu";
-		Directeur = "inconnu";		
+		Directeur =null;
+		Acteurs=null;
+		Genre=null;
+		Duree="inconnue";
 	}
+
 }
-bonjour
