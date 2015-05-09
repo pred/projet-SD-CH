6641c30be7c8b06d922d946301798c9579172e3c
pred <seb.coorevits@live.fr>
2015-05-09 14:24:05 +0200

    initialisation

---
+++ src/sd/Film.java
@@ -0,0 +1,22 @@
+package sd;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Film {
+	String Titre;
+	int Année;
+	String Type;
+	String Synopsis;
+	String Directeur;
+	List<String> Acteurs = new ArrayList<String>();
+	List<String> Genre = new ArrayList<String>();
+	
+	public Film() {
+		Titre = "inconnu";
+		Année = 0;
+		Type = "inconnu";
+		Synopsis = "inconnu";
+		Directeur = "inconnu";		
+	}
+}
